7c10b1f3be3b30024a795ae3f2b3d469f1395231
Lova-clover <woeg0226@naver.com>
2025-02-12 14:26:06 +0900

    Title: 짝수인 것만 출력하기, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250212/짝수인 것만 출력하기/print-even-number.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = list(map(int, input().split()))
+arr2 = []
+
+for i in range(n):
+    if arr[i]%2==0:
+        arr2.append(arr[i])
+
+
+for elem in arr2:
+    print(elem, end=" ")
