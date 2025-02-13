85151f5663e9c016e72fa4704a71e34ddfffc0de
Lova-clover <woeg0226@naver.com>
2025-02-13 14:35:57 +0900

    Title: 999 또는 -999, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250213/999 또는 -999/999-or-999.py
@@ -0,0 +1,11 @@
+arr = list(map(int, input().split()))
+max_val = arr[0]
+min_val = arr[0]
+
+for elem in arr:
+    if elem>max_val and elem != 999:
+        max_val = elem
+    
+    if elem<min_val and elem != -999:
+        min_val = elem
+print(max_val, min_val)
