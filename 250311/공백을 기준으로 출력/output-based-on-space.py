472c881e0d2bbf1c9e93d56befad9b0436b6b326
Lova-clover <woeg0226@naver.com>
2025-03-11 23:20:56 +0900

    Title: 공백을 기준으로 출력, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250311/공백을 기준으로 출력/output-based-on-space.py
@@ -0,0 +1,13 @@
+string1 = input()
+string2 = input()
+
+for elem in string1:
+    if elem == " ":
+        continue
+    print(elem, end='')
+
+for elem in string2:
+    if elem == " ":
+        continue
+    print(elem, end='')
+
