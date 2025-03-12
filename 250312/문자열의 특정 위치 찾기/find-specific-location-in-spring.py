5d822cb6b8a44f41ec2caf869bda6ae895d3ff62
Lova-clover <woeg0226@naver.com>
2025-03-12 22:15:10 +0900

    Title: 문자열의 특정 위치 찾기, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250312/문자열의 특정 위치 찾기/find-specific-location-in-spring.py
+++ 250312/문자열의 특정 위치 찾기/find-specific-location-in-spring.py
@@ -2,8 +2,8 @@ string = input()
 arr = ""
 s2 = string[len(string)-1]
 
-for _ in range(len(string)-2):
-    arr += string
+for i in range(len(string)-2):
+    arr += string[i]
 
 if s2 in arr:
     print(arr.index(s2))
