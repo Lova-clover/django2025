3fcf2f9a9a74c146fc2f4bb66c65246d0d00f956
Lova-clover <woeg0226@naver.com>
2025-03-13 21:49:03 +0900

    Title: 정수만 추출하기, Time: 76ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250313/정수만 추출하기/extract-only-integers.py
+++ 250313/정수만 추출하기/extract-only-integers.py
@@ -3,9 +3,11 @@ arr1, arr2 = input().split()
 for i in range(len(arr1)):
     if not arr1[i].isdigit():
         a = arr1[:i]
+        break
 
 for i in range(len(arr2)):
     if not arr2[i].isdigit():
         b = arr2[:i]
+        break
 
 print(int(a)+int(b))
