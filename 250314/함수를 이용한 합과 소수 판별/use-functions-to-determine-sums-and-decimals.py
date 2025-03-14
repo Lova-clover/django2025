49c9ed2db5afad579cac353098b4ba370b9ae280
Lova-clover <woeg0226@naver.com>
2025-03-14 23:57:40 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250314/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
@@ -0,0 +1,22 @@
+a,b = map(int,input().split())
+
+def check(num):
+    for a in range(2,num//2+1):
+        if num%a == 0:
+            return False
+    scheck = 0
+    while num != 0:
+        scheck += num%10
+        num = num//10
+    if scheck%2 != 0:
+        return False
+    return True
+
+ans = 0
+
+for t in range(a,b+1):
+    if check(t):
+        ans += 1
+
+print(ans)
+
