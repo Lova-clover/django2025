d2be6fe18d9504f3aa7713fc317cabd7fa2ea797
Lova-clover <woeg0226@naver.com>
2025-03-14 22:44:05 +0900

    Title: 함수를 이용한 소수 판별, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250314/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,26 @@
+a, b = map(int, input().split())
+
+# Please write your code here.
+
+
+def is_prime(n):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+def prime(a,b):
+    sum_val = 0
+    for i in range(a, b+1):
+        if is_prime(i):
+            sum_val +=i
+    return sum_val
+
+print(prime(a,b))
+
+
+
+
+
