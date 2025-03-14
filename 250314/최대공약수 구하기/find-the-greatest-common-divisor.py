b9be9baa657685fa2ef7f8aba7d39cef871491e3
Lova-clover <woeg0226@naver.com>
2025-03-14 21:56:23 +0900

    Title: 최대공약수 구하기, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250314/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 250314/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,9 +1,9 @@
 n, m = map(int, input().split())
-a = 0
 
 # Please write your code here.
 def nm(n,m):
-    for i in range(1, max(n,m)+1):
+    a = 0
+    for i in range(1, min(n,m)+1):
         if n%i==0 and m%i==0:
             a = i
     print(a)
