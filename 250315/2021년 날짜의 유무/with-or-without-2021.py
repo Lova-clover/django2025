edda64977fe838c694c645aebcc090bd19943a9b
Lova-clover <woeg0226@naver.com>
2025-03-15 00:18:39 +0900

    Title: 2021년 날짜의 유무, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250315/2021년 날짜의 유무/with-or-without-2021.py
+++ 250315/2021년 날짜의 유무/with-or-without-2021.py
@@ -2,7 +2,7 @@ M, D = map(int, input().split())
 
 # Please write your code here.
 
-day = [0, 31, 28, 31, 30, 31, 31, 30, 31, 30, 31]
+day = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 def YesNo(M, D):
     if (M>=1 and M<=12) and (D<=day[M]):
