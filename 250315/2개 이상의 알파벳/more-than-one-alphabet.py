038368b826f24a0924d2b06e3d9ef55f43798cd8
Lova-clover <woeg0226@naver.com>
2025-03-15 12:29:52 +0900

    Title: 2개 이상의 알파벳, Time: 50ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250315/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -0,0 +1,19 @@
+A = input()
+
+# Please write your code here.
+
+def alpa(A):
+    for i in range(len(A)):
+        cnt = 0
+        for j in range(i+1, len(A)):
+            if A[i] != A[j]:
+                cnt+=1
+        if cnt>=2:
+            return True
+    return False
+
+if alpa(A):
+    print("Yes")
+else:
+    print("No")
+
